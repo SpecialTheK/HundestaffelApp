56bc516dae3b376e904464446be5b908d308d687
Kommodore <Kommodore@users.noreply.github.com>
2018-02-02 15:34:53 +0100

    Added functionality to main menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component, ViewChild} from '@angular/core';
+import {NavController, Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
-import { Home } from '../pages/pages';
+import {Home} from '../pages/pages';
 
 @Component({
-    templateUrl: 'app.html'
+	templateUrl: 'app.html'
 })
 export class MyApp {
-    rootPage:any = Home;
-
-    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-        platform.ready().then(() => {
-            // Okay, so the platform is ready and our plugins are available.
-            // Here you can do any higher level native things you might need.
-            statusBar.styleDefault();
-            splashScreen.hide();
-        });
-    }
+	@ViewChild(NavController) navCtrl: NavController;
+	rootPage: any = Home;
+	
+	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+		platform.ready().then(() => {
+			// Okay, so the platform is ready and our plugins are available.
+			// Here you can do any higher level native things you might need.
+			statusBar.styleDefault();
+			splashScreen.hide();
+		});
+	}
+	
+	openPage(name){
+		this.navCtrl.push(name);
+	}
 }
