1fbdabaf99ed854494f431133ee59dd1b2004b92
Kommodore <Kommodore@users.noreply.github.com>
2018-05-29 16:47:59 +0200

    Fixed errors with database connection

--- src/providers/trail-storage/trail-storage.ts
+++ src/providers/trail-storage/trail-storage.ts
@@ -18,43 +18,10 @@ enum ObjectType{
 @Injectable()
 export class TrailStorageProvider {
 	//TODO: Fehler behandeln (schon inserteten stuff deleten)
-	public database = null;
+	public database;
 	
 	constructor(public storage: Storage, public platform: Platform, public sqlite: SQLite) {
-		if(this.database == null && platform.is('cordova')){
-			this.initDatabase(sqlite);
-		} else {
-			console.log('Database instance already existing or no cordova available');
-		}
-		
-		// Foreign key support
-		this.database.executeSql("PRAGMA foreign_keys = ON;", {}).then(() => {
-			console.log("Foreign key support activated.");
-		}).catch((error) => {
-			console.log("Foreign key support not activated: "+error);
-		});
-		
-		
-		//------------------------------- DEBUG START ----------------------------
-		// TODO: REMOVE
-		
-		this.database.executeSql("SELECT COUNT(*) FROM search", {}).then((data) => {
-			console.log("Search table info: "+JSON.stringify(data));
-		});
-		this.database.executeSql("SELECT COUNT(*) FROM trails", {}).then((data) => {
-			console.log("Trails table info: "+JSON.stringify(data));
-		});
-		this.database.executeSql("SELECT COUNT(*) FROM persons", {}).then((data) => {
-			console.log("Persons table info: "+JSON.stringify(data));
-		});
-		this.database.executeSql("SELECT COUNT(*) FROM map_objects", {}).then((data) => {
-			console.log("Map objects table info: "+JSON.stringify(data));
-		});
-		this.database.executeSql("SELECT COUNT(*) FROM positions", {}).then((data) => {
-			console.log("Positions table info: "+JSON.stringify(data));
-		});
-		
-		//-------------------------------  DEBUG END  ----------------------------
+		this.initDatabase(sqlite);
 	}
 
 	/**
@@ -84,7 +51,7 @@ export class TrailStorageProvider {
 	 */
 	public addSearch(trailSet: TrailSet):Promise<string>{
 		return new Promise((resolve, reject) => {
-			this.database.executeSql("INSERT INTO search VALUES ("+trailSet.creationID+", '"+trailSet.isLandTrail+"', "+trailSet.trails[0].startTime+", '"+trailSet.isTraining+"', '"+trailSet.isSharedTrail+"', '"+trailSet.preSituation+"', '"+trailSet.situation+"', "+trailSet.temperature+", '"+trailSet.precipitation+"', '"+trailSet.risks+"', '"+trailSet.person.name+"');", {}).then(() => {
+			this.database.executeSql("INSERT INTO search VALUES ("+trailSet.creationID+", '"+trailSet.isLandTrail+"', "+trailSet.trails[0].startTime.getTime()+", '"+trailSet.isTraining+"', '"+trailSet.isSharedTrail+"', '"+trailSet.preSituation+"', '"+trailSet.situation+"', "+trailSet.temperature+", '"+trailSet.precipitation+"', '"+trailSet.risks+"', '"+trailSet.person.name+"');", {}).then(() => {
 				for(let trail of trailSet.trails){
 					this.addTrailToSearch(trailSet.creationID, trail, false);
 				}
@@ -92,11 +59,12 @@ export class TrailStorageProvider {
 				this.database.executeSql("INSERT INTO persons VALUES ('"+trailSet.person.name+"', "+trailSet.person.age+", '"+trailSet.person.glasses+"', '"+trailSet.person.hair_choice+"', '"+trailSet.person.hairColor_choice+"', '"+trailSet.person.body_choice+"', '"+trailSet.person.allergies+"', '"+trailSet.person.illness+"','"+trailSet.person.medication+"','"+trailSet.person.image+"');", {}).then(() => {
 					console.log("Person inserted");
 				}).catch((error) => {
-					console.log("Error while inserting marker object: "+error);
+					console.log("Error while inserting marker object: "+JSON.stringify(error));
 				});
+				this.databaseInfo();
 				resolve("All working");
 			}).catch((error) => {
-				console.log("Error while inserting data into search table: "+error);
+				console.log("Error while inserting data into search table: "+JSON.stringify(error));
 				reject("stuff");
 			});
 		});
@@ -118,8 +86,6 @@ export class TrailStorageProvider {
 	
 	public addTrailToSet(key: number, trail:Trail, sharedActivity: boolean = false):Promise<string>{
 		return this.addTrailToSearch(key, trail, sharedActivity);
-			
-		
 	}
 	
 	/**
@@ -140,37 +106,38 @@ export class TrailStorageProvider {
 				this.database.executeSql(search_statement, {}).then((answer) => {
 					console.log("Search now marked as shared");
 				}).catch((error) => {
-					console.log("Shared not updated: "+error);
+					console.log("Shared not updated: "+JSON.stringify(error));
 				});
 			}
 			
-			this.database.executeSql("INSERT INTO trails VALUES ("+trail.id+", "+search_id+", '"+trail.trainer+"', "+trail.startTime+", "+trail.endTime+", "+trail.distance+");", {}).then(() => {
+			this.database.executeSql("INSERT INTO trails VALUES ("+trail.id+", "+search_id+", '"+trail.trainer+"', "+trail.startTime.getTime()+", "+trail.endTime.getTime()+", "+trail.distance+");", {}).then(() => {
 				for(let position of trail.path){
 					this.database.executeSql("INSERT INTO positions VALUES ("+search_id+", "+trail.id+", "+position.lng+", "+position.lat+");", {}).catch((error) => {
-						console.log("Position for trail "+trail.id+" not inserted: "+error);
+						console.log("Position for trail "+trail.id+" not inserted: "+JSON.stringify(error));
 					});
 				}
 				
 				for(let circle of trail.circles){
 					this.database.executeSql("INSERT INTO map_objects (search_id, trail_id, object_id, object_type, color, opacity, radius, pos_x1, pos_y1) VALUES ("+search_id+", "+trail.id+", "+circle.id+", "+ObjectType.CIRCLE+", '"+circle.color+"', "+circle.opacity+", "+circle.radius+", "+circle.position.lng+", "+circle.position.lat+");", {}).catch((error) => {
-						console.log("Error while inserting circle object: "+error);
+						console.log("Error while inserting circle object: "+JSON.stringify(error));
 					});
 				}
 				
 				for(let triangle of trail.triangles){
 					this.database.executeSql("INSERT INTO map_objects (search_id, trail_id, object_id, object_type, pos_x1, pos_y1, pos_x2, pos_y2, pos_x3, pos_y3) VALUES ("+search_id+", "+trail.id+", "+triangle.id+", "+ObjectType.TRIANGLE+", "+triangle.usePos[0].lng+", "+triangle.usePos[0].lat+", "+triangle.usePos[1].lng+", "+triangle.usePos[1].lat+", "+triangle.usePos[2].lng+", "+triangle.usePos[2].lat+");", {}).catch((error) => {
-						console.log("Error while inserting triangle object: "+error);
+						console.log("Error while inserting triangle object: "+JSON.stringify(error));
 					});
 				}
 				
 				for(let marker of trail.marker){
 					this.database.executeSql("INSERT INTO map_objects (search_id, trail_id, object_id, object_type, symbol_id, orientation, pos_x1, pos_y1) VALUES ("+search_id+", "+trail.id+", "+marker.id+", "+ObjectType.MARKER+", "+marker.symbolID+", "+marker.orientation+", "+marker.position.lng+", "+marker.position.lat+");", {}).catch((error) => {
-						console.log("Error while inserting marker object: "+error);
+						console.log("Error while inserting marker object: "+JSON.stringify(error));
 					});
 				}
+				this.databaseInfo();
 				resolve("Trail inserted");
 			}).catch((error) => {
-				reject("Failed to insert trail with id "+trail.id+": "+error);
+				reject("Failed to insert trail with id "+trail.id+": "+JSON.stringify(error));
 			});
 		});
 	}
@@ -205,8 +172,11 @@ export class TrailStorageProvider {
 	public deleteSearch(search_id: number):Promise<string>{
 		return new Promise((resolve, reject) => {
 			this.database.executeSql("DELETE FROM search WHERE search_id = "+search_id+";", {})
-				.then( () => resolve("Search deleted"))
-				.catch((error) => reject("Search not deleted: "+error));
+				.then( () => {
+					resolve("Search deleted");
+					this.databaseInfo();
+				})
+				.catch((error) => reject("Search not deleted: "+JSON.stringify(error)));
 		});
 	}
 
@@ -230,10 +200,10 @@ export class TrailStorageProvider {
 					}
 					resolve(TrailSet.fromData(JSON.parse(answer)));
 				}).catch((error) => {
-					reject("Couldn't get trailSet: "+error);
+					reject("Couldn't get trailSet: "+JSON.stringify(error));
 				});
 			}).catch((error) => {
-				reject("Internal storage error: "+error);
+				reject("Internal storage error: "+JSON.stringify(error));
 			});
 		});
 	}
@@ -264,11 +234,11 @@ export class TrailStorageProvider {
 								observer.complete();
 							}
 						}).catch((error) => {
-							observer.error("Couldn't fetch trailSet: "+error);
+							observer.error("Couldn't fetch trailSet: "+JSON.stringify(error));
 						});
 					}
 				}).catch((error) => {
-					observer.error("Couldn't fetch keys: "+error);
+					observer.error("Couldn't fetch keys: "+JSON.stringify(error));
 				});
 			});
 		});
@@ -286,51 +256,79 @@ export class TrailStorageProvider {
 		sqlite.create({name: 'trailStorage.db', location: 'default'}).then((database: SQLiteObject) => {
 			this.database = database;
 			
+			// Foreign key support
+			this.database.executeSql("PRAGMA foreign_keys = ON;", {}).then(() => {
+				console.log("Foreign key support activated.");
+			}).catch((error) => {
+				console.log("Foreign key support not activated: "+JSON.stringify(error));
+			});
+			
 			// Search table
 			this.database.executeSql("CREATE TABLE IF NOT EXISTS `search` ( search_id NUMERIC, search_type TEXT, search_date NUMERIC, is_training TEXT, is_shared TEXT, pre_situation TEXT, situation TEXT, temperature REAL, precipitation TEXT, risks TEXT, person_name TEXT, PRIMARY KEY (search_id) );", {}).then(() => {
 				console.log("Search table inserted");
 			}).catch((error) => {
-				console.log("Search table not inserted: "+error);
+				console.log("Search table not inserted: "+JSON.stringify(error));
 			});
 			
 			// Trails table
 			this.database.executeSql("CREATE TABLE IF NOT EXISTS `trails` ( trail_id INTEGER, search_id NUMERIC, trainer TEXT, start_time NUMERIC, end_time NUMERIC, distance REAL, FOREIGN KEY (search_id) REFERENCES search (search_id) ON DELETE CASCADE, PRIMARY KEY (search_id, trail_id) );", {}).then(() => {
 				console.log("Trails table inserted");
 			}).catch((error) => {
-				console.log("Trails table not inserted: "+error);
+				console.log("Trails table not inserted: "+JSON.stringify(error));
 			});
 			
 			// Positions table
 			this.database.executeSql("CREATE TABLE IF NOT EXISTS `positions` ( search_id NUMERIC, trail_id INTEGER, pos_x REAL, pos_y REAL, FOREIGN KEY (search_id) REFERENCES search (search_id) ON DELETE CASCADE, FOREIGN KEY (trail_id) REFERENCES trails (trail_id) ON DELETE CASCADE );", {}).then(() => {
 				console.log("Positions table inserted");
 			}).catch((error) => {
-				console.log("Positions table not inserted: "+error);
+				console.log("Positions table not inserted: "+JSON.stringify(error));
 			});
 			
 			// Map objects table
 			this.database.executeSql("CREATE TABLE IF NOT EXISTS `map_objects` ( search_id NUMERIC, trail_id INTEGER, object_id INTEGER, object_type INTEGER, color TEXT, opacity REAL, radius REAL, orientation REAL, symbol_id INTEGER, pos_x1 REAL, pos_y1 REAL, pos_x2 REAL, pos_y2 REAL, pos_x3 REAL, pos_y3 REAL, FOREIGN KEY (search_id) REFERENCES search (search_id) ON DELETE CASCADE, FOREIGN KEY (trail_id) REFERENCES trails (trail_id) ON DELETE CASCADE, PRIMARY KEY (search_id, trail_id, object_id) );", {}).then(() => {
 				console.log("Map objects table inserted");
 			}).catch((error) => {
-				console.log("Map objects table not inserted: "+error);
+				console.log("Map objects table not inserted: "+JSON.stringify(error));
 			});
 			
 			// Persons table
 			this.database.executeSql("CREATE TABLE IF NOT EXISTS `persons` ( name TEXT, age INTEGER, glasses TEXT, hair_style TEXT, hair_color TEXT, body_type TEXT, allergies TEXT, illness TEXT, medication TEXT, image TEXT );", {}).then(() => {
 				console.log("Persons table inserted");
 			}).catch((error) => {
-				console.log("Persons table not inserted: "+error);
+				console.log("Persons table not inserted: "+JSON.stringify(error));
 			});
+			
+			this.databaseInfo();
 		}).catch((error) => {
 			console.log('Database not opened: '+JSON.stringify(error));
 		});
 	}
 	
+	private databaseInfo(){
+		this.database.executeSql("SELECT COUNT(*) FROM search", {}).then((data) => {
+			console.log("Search table info: "+JSON.stringify(data));
+		});
+		this.database.executeSql("SELECT COUNT(*) FROM trails", {}).then((data) => {
+			console.log("Trails table info: "+JSON.stringify(data));
+		});
+		this.database.executeSql("SELECT COUNT(*) FROM persons", {}).then((data) => {
+			console.log("Persons table info: "+JSON.stringify(data));
+		});
+		this.database.executeSql("SELECT COUNT(*) FROM map_objects", {}).then((data) => {
+			console.log("Map objects table info: "+JSON.stringify(data));
+		});
+		this.database.executeSql("SELECT COUNT(*) FROM positions", {}).then((data) => {
+			console.log("Positions table info: "+JSON.stringify(data));
+		});
+	}
+	
 	//TODO Debug -> Entfernen!!!!!
 	public deleteDatabase(){
+		this.databaseInfo();
 		this.sqlite.deleteDatabase({name: 'trailStorage.db', location: 'default'}).then( (answer) => {
 			console.log("Database deleted: "+answer);
 		}).catch((error) => {
-			console.log("Database not deleted: "+error);
+			console.log("Database not deleted: "+JSON.stringify(error));
 		})
 	}
 }
